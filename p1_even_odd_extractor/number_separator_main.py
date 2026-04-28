4d04c9debd774377dcba6888656b25e5e040a461
Samuel <samueldavetoledo678@gmail.com>
2026-04-28 22:40:48 +0800

    Fixed an issue where txt data files are created outside of the project file's folder, to avoid confusion for future file handling. Made sure that the txt data files are created within the project files instead of the main file.

--- p1_even_odd_extractor/number_separator_main.py
+++ p1_even_odd_extractor/number_separator_main.py
@@ -1,16 +1,22 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+current_dir = os.path.dirname(__file__)
+
+sys.path.append(os.path.abspath(os.path.join(current_dir, '..')))
 from shared_functionalities_or_utilities import ProjectHelper
 from extractor_methods import NumberSeparator
 
 def main():
     ProjectHelper.print_header("Program 1: Even and Odd Extractor")
     
-    ProjectHelper.create_dummy_numbers("numbers.txt")
+    input_file = os.path.join(current_dir, "numbers.txt")
+    even_file = os.path.join(current_dir, "even.txt")
+    odd_file = os.path.join(current_dir, "odd.txt")
     
-    extractor = NumberSeparator("numbers.txt", "even.txt", "odd.txt")
+    ProjectHelper.create_dummy_numbers(input_file)
+    
+    extractor = NumberSeparator(input_file, even_file, odd_file)
     extractor.extract_numbers()
     print("[+] Numbers successfully extracted to even.txt and odd.txt")
 

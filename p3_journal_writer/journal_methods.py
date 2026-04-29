189ccfaa1dad7dffa9a4ce067370a69bf4b97833
Samuel <samueldavetoledo678@gmail.com>
2026-04-29 12:08:16 +0800

    Improved out put of p3  journal, indicated date of entry and improved readability

--- p3_journal_writer/journal_methods.py
+++ p3_journal_writer/journal_methods.py
@@ -1,15 +1,23 @@
+from datetime import datetime
+
 class InteractiveJournal:
     def __init__(self, output_file):
         self.output_file = output_file
 
     def write_entries(self):
-        with open(self.output_file, 'w') as file:
+        with open(self.output_file, 'a') as file:
+            
+            current_date = datetime.now().strftime("%A, %B %d, %Y")
+            file.write("\n" + "="*40 + "\n")
+            file.write(f" ENTRY DATE: {current_date}\n")
+            file.write("="*40 + "\n")
+            
             while True:
                 line = input("Enter line: ")
-                file.write(line + "\n")
+                file.write(f" • {line}\n")
                 
                 more_lines = input("Are there more lines y/n? ").strip().lower()
                 if more_lines != 'y':
                     break
-                
-        print(f"[+] Journal saved to {self.output_file}")
+                    
+        print(f"[+] Journal updated securely at {self.output_file}")
